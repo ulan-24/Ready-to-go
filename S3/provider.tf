b66d2fba6fd60c9f76b5649e4d6f83216d0b9b6e
gaukhar97 <69063982+gaukhar97@users.noreply.github.com>
2020-09-03 16:44:48 -0500

    Update provider.tf

--- S3/provider.tf
+++ S3/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-west-2" #Oregon
-  access_key = "AKIAZ6FDPN6WMRJQAEHK"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
